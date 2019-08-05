83d09b8b2fb2d1ea20a6c288a7f85f5e098161b3
juliank <julian.kanjere@jskdezign.com>
2019-08-05 09:48:24 +0200

    Update CoToken deployment script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,6 @@
 const CoToken = artifacts.require("./CoToken.sol");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(CoToken, "CO", "CoTokenSymbol")
+  await deployer.deploy(CoToken)
   const erc20 = await CoToken.deployed()
 };
